7fe4193c92d86d160832b169f5b6d515fb0612a3
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-01-31 11:45:13 +0100

    exercise 1 & 2

--- 1._Basics.js
+++ 1._Basics.js
@@ -8,7 +8,8 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
-
+const introduction = `My first name is ${firstName} and my last name is ${lastName}`;
+console.log(introduction);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -19,6 +20,5 @@ const increment = 1;
 // Add the year plus the increment
 // The result should be 2025
 // You cannot touch the first or the second line
-
-
+console.log(parseInt(year) + increment)
 // --------------------------------------
