08d3a16fffd3f6e17056789ffef76612018eb92a
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-03 15:25:27 +0100

    object exercises

--- 3._Objects.js
+++ 3._Objects.js
@@ -6,12 +6,15 @@
 const myObject = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
-
+console.log(myObject.message)
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
+const myInfo = {
+    name: "Lasse",
+    age: 21
+}
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair 
@@ -19,15 +22,15 @@ const myObject = { message: "Hello, earthling! I bring peace." };
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
-
+stackOverflow.isAllowed = true;
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
-
-
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
 // --------------------------------------
 
 
